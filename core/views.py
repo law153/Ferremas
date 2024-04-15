9ec8cdcad77cb115ba44001f27f1b6880824d5a1
Abel <abelx3678@gmail.com>
2024-04-15 19:10:19 -0400

    agregado consumir la api con detalles
    no se esta mostrando el resultado de la api en el html

--- core/views.py
+++ core/views.py
@@ -20,8 +20,6 @@ def mostrarIndex(request):
     return render(request, 'core/index.html')
 
 
-
-
 def obtener_categorias(request):
     # URL del servicio web desde el cual deseas obtener los datos
     url_servicio_web = 'http://localhost:8000/api/categorias/'
@@ -30,13 +28,24 @@ def obtener_categorias(request):
     response = requests.get(url_servicio_web)
 
     if response.status_code == 200:
-       
+        # Obtén los datos en formato JSON
         datos = response.json()
-    else:
-        
-        datos = {'error': 'No se pudieron obtener los datos del servicio web'}
+        print('Datos obtenidos:', datos)
+        # Instancia el serializer con los datos recibidos
+        serializer = categoriaSerializer(data=datos['categorias'], many=True)
 
-    return render(request, 'core/index.html', {'datos': datos})
+        # Verifica si los datos son válidos según el serializer
+        if serializer.is_valid():
+            # Obtiene los datos serializados
+            categorias_serializadas = serializer.data
+        else:
+            # Si los datos no son válidos, muestra un error
+            categorias_serializadas = {'error': 'Error al procesar los datos del servicio web'}
+    else:
+        categorias_serializadas = {'error': 'No se pudieron obtener los datos del servicio web'}
+
+    # Pasa los datos serializados a la plantilla
+    return render(request, 'core/index.html', {'categorias_serializadas': categorias_serializadas})
 
 ###Cliente
 
