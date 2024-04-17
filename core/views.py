3cabfbbefe338a374bcc13e027861290794f34fb
Abel <abelx3678@gmail.com>
2024-04-17 16:24:04 -0400

    las categorias se toman desde el servicio web

--- core/views.py
+++ core/views.py
@@ -17,35 +17,20 @@ class listaCategoriasApi(generics.ListAPIView):
 ###No hay cuenta
 def mostrarIndex(request):
 
-    return render(request, 'core/index.html')
+    categorias = obtener_categorias()
+
+    contexto = {"categorias" : categorias}
+
+    return render(request, 'core/index.html',contexto)
 
 
-def obtener_categorias(request):
-    # URL del servicio web desde el cual deseas obtener los datos
-    url_servicio_web = 'http://localhost:8000/api/categorias/'
-
-    # Realiza una solicitud GET al servicio web
-    response = requests.get(url_servicio_web)
-
-    if response.status_code == 200:
-        # Obtén los datos en formato JSON
-        datos = response.json()
-        print('Datos obtenidos:', datos)
-        # Instancia el serializer con los datos recibidos
-        serializer = categoriaSerializer(data=datos['categorias'], many=True)
-
-        # Verifica si los datos son válidos según el serializer
-        if serializer.is_valid():
-            # Obtiene los datos serializados
-            categorias_serializadas = serializer.data
-        else:
-            # Si los datos no son válidos, muestra un error
-            categorias_serializadas = {'error': 'Error al procesar los datos del servicio web'}
+def obtener_categorias():
+    url_servicio = 'http://localhost:8000/api/categorias/'
+    respuesta = requests.get(url_servicio)
+    if respuesta.status_code == 200:
+        return respuesta.json()
     else:
-        categorias_serializadas = {'error': 'No se pudieron obtener los datos del servicio web'}
-
-    # Pasa los datos serializados a la plantilla
-    return render(request, 'core/index.html', {'categorias_serializadas': categorias_serializadas})
+        return None
 
 ###Cliente
 
